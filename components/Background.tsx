662877654a3bc3d85e6505cf11c31b2e080a9528
KyleSmith0905 <kylesmith090502@gmail.com>
2021-12-08 14:31:03 -0500

    Optimization: Moved HTML5 Canvas to SVG. SVG is ~50% faster on tests.

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,37 +1,50 @@
-import { FunctionComponent, useEffect } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import { IBubble } from '../lib/bubbleData/_shared';
 import { IsArrayNaN } from '../lib/utils';
 
 const Circle: FunctionComponent<{bubbles: IBubble[]}> = ({bubbles}) => {
 
-	useEffect(() => {		
-		const timer = setInterval(() => {
-			const background = document.getElementById('Background') as HTMLCanvasElement;
+	const [path, setPath] = useState('');
 
-			background.width = document.body.clientWidth;
-			background.height = document.body.clientHeight;
-	
-			const context = background.getContext('2d');
-			if (context === null) return
-			context.clearRect(0, 0, background.width, background.height);
-	
-			context.beginPath();
+	useEffect(() => {
+		let requestId = 0;
+
+		const positionToString = (position: number[]): string => {
+			return (position[0] * 20).toFixed(2) + ',' + (position[1] * 20).toFixed(2);
+		}
+
+		const renderBackground = () => {
+
+			let localPath = '';
+
 			for (let i = 0; i < bubbles.length; i++) {
 				const bubble = bubbles[i];
 				const connection = bubbles.find(e => e.id === bubble.connection);
 				if (connection === undefined || IsArrayNaN(connection.position) || IsArrayNaN(bubble.position)) continue;
 
-				context.moveTo(bubble.position[0] * 20, bubble.position[1] * 20);
-				context.lineTo(connection.position[0] * 20, connection.position[1] * 20);
+				localPath += 'M' + positionToString(connection.position) + 'L' + positionToString(bubble.position);
 			}
-			context.stroke();
-		}, 30);
-		
-		return () => clearInterval(timer);
+			
+			setPath(localPath);
+			requestId = requestAnimationFrame(renderBackground);
+		};
+
+		requestId = requestAnimationFrame(renderBackground);
+		return () => cancelAnimationFrame(requestId);
 	});
 
 	return (
-		<canvas id='Background'/>
+		<svg
+			id='Background'
+			height={2000}
+			width={2000}
+		>
+			<path
+				d={path}
+				stroke='black'
+				strokeWidth={2}
+			/>
+		</svg>
 	)
 }
 
