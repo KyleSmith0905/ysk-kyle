dcdc01d909bf747691cdcabb09a1aac4a96e0f19
KyleSmith0905 <kylesmith090502@gmail.com>
2021-12-18 15:16:23 -0500

    Feature: removed the useRouter in favor of passing the path down from functions.

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,8 +1,7 @@
 import { readdirSync } from 'fs';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import Background from '../components/Background';
 import Bubble from '../components/Bubble';
 import BrowserMovement from '../components/MovementControl/Browser';
@@ -13,12 +12,8 @@ import Settings from '../components/Settings';
 import { IBubble } from '../lib/bubbleData/_shared';
 
 
-const BubblePage: NextPage = () => {
-  const router = useRouter();
-  let slug = router.query.slug;
-  if (router.asPath === '/') slug = 'homepage';
-  else if (typeof(slug) !== 'string') slug = '404';
-  const slugFormated = slug.split('-').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
+const BubblePage: NextPage<{slug: string}> | FunctionComponent<{slug: string}> = ({slug}) => {
+  const slugFormated = slug.split('-').map((s: string) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
 
   const [bubbles, setBubbles] = useState<IBubble[]>([]);
   const [travelMode, setTravelMode] = useState(0);
@@ -32,15 +27,14 @@ const BubblePage: NextPage = () => {
       const imported = (await import('../lib/bubbleData/' + slug));
       setBubbles(imported.default.reverse());
     })();
-  }, [slug, router.pathname]);
+  }, [slug]);
 
   return (
     <>
       <Head>
-        <title>{slugFormated + '| YSK Kyle - A portfolio website for Kyle Smith'}</title>
+        <title>{slugFormated + ' | YSK Kyle - A portfolio website for Kyle Smith'}</title>
         <meta property='og:title' content={slugFormated + ' | YSK Kyle - A portfolio website for Kyle Smith'} />
         <meta name='twitter:title' content={slugFormated + ' | YSK Kyle - A portfolio website for Kyle Smith'} />
-        <link rel='canonical' href={'https://yskkyle/' + slug}/>
       </Head>
       <Background
         bubbles={bubbles}
@@ -71,9 +65,11 @@ export default BubblePage;
 
 export const getStaticPaths: GetStaticPaths = async () => {
   let allPaths = readdirSync('lib/bubbleData');
-  allPaths = allPaths.map(path => path.split('.')[0]);
+  
+	allPaths = allPaths.map(path => path.split('.')[0]);
   allPaths = allPaths.filter(path => !path.startsWith('_'));
-  allPaths= allPaths.filter(path => path !== '404');
+  allPaths = allPaths.filter(path => path !== '404');
+	allPaths = allPaths.filter(path => path !== 'index');
 
   const paramsFormat = allPaths.map(path => ({ params: { slug: path } }));
 
