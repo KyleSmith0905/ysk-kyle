3726b607a6e70fcb8c9b3b60ebd78d2765997d89
KyleSmith0905 <kylesmith090502@gmail.com>
2021-12-18 16:14:46 -0500

    Meta: Test no longer test if / and /homepage are equal... /homepage is deprecated.

--- cypress/integration/pages.spec.ts
+++ cypress/integration/pages.spec.ts
@@ -3,25 +3,15 @@ describe('Pages', () => {
 		cy.request({url: '/lorem-ipsum', failOnStatusCode: false}).its('status').should('eq', 404);
 
 		cy.visit('/lorem-ipsum', {failOnStatusCode: false});
-		cy.get('#center > h2').should('have.text', '404: Page Not Found');
+		cy.get('#center > h2').should('have.text', 'An Error Occurred!');
 	});
 
 	it('404 on `./404`', () => {
 		cy.request({url: '404', failOnStatusCode: false}).its('status').should('eq', 404);
 
 		cy.visit('/404', {failOnStatusCode: false});
-		cy.get('#center > h2').should('have.text', '404: Page Not Found');
+		cy.get('#center > h2').should('have.text', 'An Error Occurred!');
 	});
-
-	it('homepage on `./` and `./homepage`', () => {
-		cy.visit('/');
-		cy.get('#welcome > h2').should('have.text', 'You Should Know Kyle!');
-		cy.get('link[rel="canonical"]').invoke('attr', 'href').should('eq', 'https://yskkyle/homepage');
-		
-		cy.visit('/homepage');
-		cy.get('#welcome > h2').should('have.text', 'You Should Know Kyle!');
-	});
-
 
 	it('`./[slug]`', () => {
 		cy.visit('/sorting-algorithm-visualizer');
