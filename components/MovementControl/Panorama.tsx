f2b9463e84861371a28d9fb0f1efb635d3b9f9ca
KyleSmith0905 <kylesmith090502@gmail.com>
2021-12-18 20:35:03 -0500

    Fix: Settings menu remains a consistent size.

--- components/MovementControl/Panorama.tsx
+++ components/MovementControl/Panorama.tsx
@@ -3,13 +3,25 @@ import { FunctionComponent, useEffect } from 'react';
 const PanoramaMovement: FunctionComponent = () => {
 
   useEffect(() => {
-		const widthScale = window.innerWidth / 2000;
-		const heightScale = window.innerHeight / 2000;
-    scrollTo(1000 - (window.innerWidth / 2), 1000 - (window.innerHeight / 2));
-		document.body.style.transform = 'scale(' + Math.min(widthScale, heightScale) + ')';
-
+		const background = document.getElementById('Background');
+		const content = document.getElementById('MainContent');
+		if (background === null || content === null) return;
+		
+		const setScale = () => {
+			const widthScale = window.innerWidth / 2000;
+			const heightScale = window.innerHeight / 2000;
+			scrollTo(1000 - (window.innerWidth / 2), 1000 - (window.innerHeight / 2));
+			background.style.transform = 'scale(' + Math.min(widthScale, heightScale) + ')';
+			content.style.transform = 'scale(' + Math.min(widthScale, heightScale) + ')';
+		};
+		setScale();
+		
+		window.addEventListener('resize', setScale);
+		
 		return () => {
-			document.body.style.transform = 'none';
+			window.removeEventListener('resize', setScale);
+			background.style.transform = 'none';
+			content.style.transform = 'none';
 		};
   }, []);
 
