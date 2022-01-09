7bde82641cc631ff10b055922295b6bd9a7ba770
KyleSmith0905 <kylesmith090502@gmail.com>
2022-01-09 14:17:21 -0500

    Fix: Service works no longer brake when caching extensions.

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,18 +3,17 @@ var CACHE_NAME = 'ysk-kyle-cache-v1';
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      }
-
+      if (response) return response;
+      
       return fetch(event.request).then(function(response) {
-        if(!response || response.status !== 200 || response.type !== 'basic') {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
           return response;
         }
 
         var responseToCache = response.clone();
 
         caches.open(CACHE_NAME).then(function(cache) {
+          if (!event.request.url.startsWith('http')) return;
           cache.put(event.request, responseToCache);
         });
 
