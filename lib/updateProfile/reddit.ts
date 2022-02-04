a891b9c700a9610ecd32058685e2ad4a38a178e5
KyleSmith0905 <kylesmith090502@gmail.com>
2022-02-03 23:02:28 -0500

    Feature: Now updates reddit profile picture on `/api/update-profile`

--- lib/updateProfile/reddit.ts
+++ lib/updateProfile/reddit.ts
@@ -1,8 +1,6 @@
 import { Readable } from 'stream';
 import FormData from 'form-data';
 
-// OUT OF IMPLEMENTATION
-// Returns 503 Service Unavailable, meaning, it's temporarily unavailable.
 const ChangeProfile = async (picture: Buffer) => {
 	if (!process.env.REDDIT_APP_SECRET
 		|| !process.env.REDDIT_APP_ID
@@ -15,37 +13,29 @@ const ChangeProfile = async (picture: Buffer) => {
 		headers: {
 			'Authorization': 'Basic ' + Buffer.from(process.env.REDDIT_APP_ID + ':' + process.env.REDDIT_APP_SECRET).toString('base64'),
 			'Content-Type': 'application/x-www-form-urlencoded',
-			'User-Agent': 'script:com.yskkyle.UpdateProfile:1.0 (by /u/' + process.env.REDDIT_USERNAME + ')',
+			'User-Agent': 'script:com.yskkyle.UpdateProfile:1.1 (by /u/' + process.env.REDDIT_USERNAME + ')',
 		},
-		body: 'grant_type=password&username=' + encodeURI(process.env.REDDIT_USERNAME) + '&password=' + encodeURI(process.env.REDDIT_PASSWORD ?? ''),
+		body: 'grant_type=password&username=' + encodeURI(process.env.REDDIT_USERNAME) + '&password=' + encodeURI(process.env.REDDIT_PASSWORD),
 	});
 
 	const token = await res.json();
 	
-	const readable = new Readable({
-		read() {
-			this.push(picture);
-		}
-	});
-
 	const fileData: { [key: string]: string | Readable } = {
 		upload_type: 'icon',
 		img_type: 'png',
-		file: readable,
 	};
-
+	
 	const requestBody = new FormData();
 	Object.keys(fileData).forEach(key => requestBody.append(key, fileData[key]));
 	
-	// console.log('https://oauth.reddit.com/r/u_' + encodeURI(process.env.REDDIT_USERNAME) + '/api/upload_sr_img');
-	// console.log('script:com.yskkyle.UpdateProfile:1.0 (by /u/' + process.env.REDDIT_USERNAME + ')');
-	// console.log(requestBody);
-
-	fetch('https://oauth.reddit.com/r/u_' + encodeURI(process.env.REDDIT_USERNAME) + '/api/upload_sr_img', {
+	const parsedImage = Readable.from(picture);
+	requestBody.append('file', parsedImage, 'avatar.png');
+	
+	fetch('https://oauth.reddit.com/r/u_' + process.env.REDDIT_USERNAME + '/api/upload_sr_img', {
 		method: 'POST',
 		headers: {
-			'Authorization': 'bearer ' + token.access_token,
-			'User-Agent': 'script:com.yskkyle.UpdateProfile:1.0 (by /u/' + process.env.REDDIT_USERNAME + ')',
+			'Authorization': 'Bearer ' + token.access_token,
+			'User-Agent': 'script:com.yskkyle.UpdateProfile:1.1 (by /u/' + process.env.REDDIT_USERNAME + ')',
 		},
 		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		// @ts-ignore
--- pages/api/update-profile.ts
+++ pages/api/update-profile.ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import createIdenticon from 'cyber-circle-identicon';
+import createIdenticon, { AdditionalOptions } from 'cyber-circle-identicon';
 import crypto from 'crypto';
 import setDiscord from '../../lib/updateProfile/discord';
 import setTwitter from '../../lib/updateProfile/twitter';
 import setGravatar from '../../lib/updateProfile/gravatar';
 import setYoutube from '../../lib/updateProfile/youtube';
-// import setReddit from '../../lib/updateProfile/reddit';
+import setReddit from '../../lib/updateProfile/reddit';
 
 const UpdateProfile = async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method !== 'POST') {
@@ -26,7 +26,7 @@ const UpdateProfile = async (req: NextApiRequest, res: NextApiResponse) => {
 	}
 
 	const identiconKey = crypto.randomBytes(8);
-	const identiconSettings = {
+	const identiconSettings: AdditionalOptions = {
 		size: 256,
 		clipped: false,
 		overrideData: {
@@ -38,7 +38,8 @@ const UpdateProfile = async (req: NextApiRequest, res: NextApiResponse) => {
 				start: { r: 255, g: 192, b: 0 },
 				end: { r: 255, g: 63, b: 0 },
 			}
-		}
+		},
+		compression: 6,
 	};
 
 	const identicon = createIdenticon(identiconKey.toString('utf8'), identiconSettings);
@@ -49,9 +50,15 @@ const UpdateProfile = async (req: NextApiRequest, res: NextApiResponse) => {
 		const youtubePromise = setYoutube(clippedIdenticon);
 		const twitterPromise = setTwitter(identicon);
 		const gravatarPromise = setGravatar(identicon);
-		// const redditPromise = setReddit(identicon);
+		const redditPromise = setReddit(identicon);
 		
-		await Promise.all([discordPromise, youtubePromise, twitterPromise, gravatarPromise]);
+		await Promise.all([
+			discordPromise,
+			youtubePromise,
+			twitterPromise,
+			gravatarPromise,
+			redditPromise
+		]);
 	}
 	catch (err) {
 		res.writeHead(500);
