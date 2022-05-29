b204c4637120add3d542d4ed82c903cb01c175ed
KyleSmith0905 <kylesmith090502@gmail.com>
2022-05-29 13:01:21 -0400

    Feature: Lowered bundle size by half using Preact.

--- next.config.mjs
+++ next.config.mjs
@@ -30,6 +30,17 @@ const configSettings = {
 			},
 		];
 	},
+	webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 };
 
 export default configSettings;
