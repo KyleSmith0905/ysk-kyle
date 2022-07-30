12eb34fb6a1d819ee6e407e11f7bbc3443182e02
KyleSmith0905 <kylesmith090502@gmail.com>
2022-07-30 12:47:44 -0400

    Fix: Preact hasn't been working, temporarily removed it.

--- next.config.mjs
+++ next.config.mjs
@@ -30,17 +30,6 @@ const configSettings = {
 			},
 		];
 	},
-	webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
-    return config;
-  },
 };
 
 export default configSettings;
