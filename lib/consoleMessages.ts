3c0013fcb96eaf92c1ff8f5e488a2971ec42d20f
KyleSmith0905 <kylesmith090502@gmail.com>
2022-07-31 08:28:50 -0400

    Feature: Added welcome message in the console.

---
+++ lib/consoleMessages.ts
@@ -0,0 +1,16 @@
+const welcomeMessage = () => {
+	// In the future, refer to this: https://patorjk.com/software/taag/#p=display&h=0&f=Doom&t=KyleSmith0905
+	return `\
+Hello fellow developer! Looking for how this was made?
+Source Code: https://github.com/KyleSmith0905/ysk-kyle.
+ _   __        _        _____             _  _    _      _____  _____  _____  _____ 
+| | / /       | |      /  ___|           (_)| |  | |    |  _  ||  _  ||  _  ||  ___|
+| |/ /  _   _ | |  ___ \\ \`--.  _ __ ___   _ | |_ | |__  | |/' || |_| || |/' ||___ \\ 
+|    \\ | | | || | / _ \\ \`--. \\| '_ \` _ \\ | || __|| '_ \\ |  /| |\\____ ||  /| |    \\ \\
+| |\\  \\| |_| || ||  __//\\__/ /| | | | | || || |_ | | | |\\ |_/ /.___/ /\\ |_/ //\\__/ /
+\\_| \\_/ \\__, ||_| \\___|\\____/ |_| |_| |_||_| \\__||_| |_| \\___/ \\____/  \\___/ \\____/ 
+         __/ |                                                                      
+        |___/`;
+};
+
+export {welcomeMessage};
