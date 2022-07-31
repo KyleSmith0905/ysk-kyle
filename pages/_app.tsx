1398894f2ee22fc394926dc259a74b2331feb6a1
KyleSmith0905 <kylesmith090502@gmail.com>
2022-07-31 08:36:02 -0400

    Fix: Property of undefined error in _app.tsx about cookieColorModes.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,7 +47,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <meta name='twitter:title' content='YSK Kyle - A portfolio website for Kyle Smith' />
         <meta name='twitter:description' content='YSK Kyle is a portfolio website for Kyle Smith to showcase his web design and programming experience.' />
       </Head>
-      <div id='ColorTheme' className={cookiesColorMode.name}>
+      <div id='ColorTheme' className={cookiesColorMode?.name}>
         <Component {...pageProps} />
       </div>
     </>
