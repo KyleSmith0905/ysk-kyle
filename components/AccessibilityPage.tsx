8853ee526f64e25d88b5316080ef2b0c4036f9db
Kyle Smith <kylesmith090502@gmail.com>
2023-01-24 17:14:15 -0500

    Accessibility page follows graph data.

--- components/AccessibilityPage.tsx
+++ components/AccessibilityPage.tsx
@@ -6,6 +6,55 @@ import { IBubble } from '../lib/bubbleData/_shared';
 import { COLOR_MODES } from '../lib/colorMode';
 import { Cookies } from '../lib/cookies';
 
+interface RecursiveBubble {
+  bubble: IBubble,
+  children: RecursiveBubble[];
+}
+
+const InfoBlock: FunctionComponent<{
+  recursiveBubble: RecursiveBubble;
+}> = ({
+  recursiveBubble,
+}) => {
+  const bubble = recursiveBubble.bubble;
+
+  return (
+    <div key={bubble.id} className='bubbleContainer'>
+      {bubble.summary !== undefined && (
+        <div>
+          {bubble.size === 'large' && <h1>{bubble.name}</h1>}
+          {(bubble.size === 'medium' || !bubble.size) && <h2>{bubble.name}</h2>}
+          {bubble.size === 'small' && <h3>{bubble.name}</h3>}
+          {bubble.link && <Link href={bubble.link}>[More Information]</Link>}
+          <br />
+          <p>{bubble.summary}</p>
+        </div>
+      )}
+      {bubble.summary === undefined && (
+        <div className='image'>
+          <Image
+            src={'/images/' + bubble.image + '.png'}
+            alt={bubble.name}
+            quality={50}
+            priority={true}
+            height={160}
+            layout='fill'
+            objectFit='contain'
+          />
+        </div>
+      )}
+      <div className='children'>
+        <div className='connections'>
+
+        </div>
+        {recursiveBubble.children?.map((children) => (
+          <InfoBlock key={children.bubble.id} recursiveBubble={children}/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 const AccessibilityPage: FunctionComponent<{
   slug: string;
   bubbles: IBubble[];
@@ -15,71 +64,85 @@ const AccessibilityPage: FunctionComponent<{
   bubbles: localBubble = [], setAccessibility, cookies,
 }) => {
 
-  const [reformattedBubbles, setReformattedBubbles] = useState(localBubble);
+    const [reformattedBubbles, setReformattedBubbles] = useState<RecursiveBubble>();
 
-  // Uses cookie values to save settings.
-  useEffect(() => {
-    const root = document.getElementById('ColorTheme');
-    if (root) root.className = 'Light';
-  }, [cookies]);
+    // Uses cookie values to save settings.
+    useEffect(() => {
+      const root = document.getElementById('ColorTheme');
+      if (root) root.className = 'Light';
+    }, [cookies]);
 
-  // Reformats the bubble so they look like they're in the correct order.
-  useEffect(() => {
-    // const bubbleSortingPoints: Record<number, IBubble> = {};
-    // const core = localBubble.find((e) => e.connection === '.');
+    // Reformats the bubble so they look like they're in the correct order.
+    useEffect(() => {
+      const unusedBubbles = [...localBubble];
 
-    // // Any bubble that is small connect to a core should be first, then any medium ones.
-    // for (const bubble of localBubble) {
-    //   const connection = localBubble.find((e) => e.id === bubble.connection)
-    // }
+      const centralBubble = unusedBubbles.findIndex((e) => e.connection === '.');
+      const reformattedBubbles: RecursiveBubble = {bubble: unusedBubbles[centralBubble], children: []};
 
-    setReformattedBubbles(localBubble);
-  }, [localBubble]);
+      // Keeps track of available spaces that could take children.
+      let availableChildren: [string[][], IBubble[]] = [[], []];
+      let nextAvailableChildren: [string[][], IBubble[]] = [[[]], [unusedBubbles[centralBubble]]];
 
-  return (
-    <div className='accessibilityPage'>
-      <Head>
-        <meta name='theme-color' content={COLOR_MODES.find(e => e.name === 'Light')?.primary} />
-      </Head>
-      <header>
-        <p className='logo'>YSK Kyle</p>
-        <div className='buttonRow'>
-          <Link href='/'>
-            <button className='button'>Home Page</button>
-          </Link>
-          <button className='button' onClick={() => setAccessibility('Visuals')}>To Visual Page</button>
-        </div>
-      </header>
-      <main>
-        {reformattedBubbles.map((bubble) => (
-          <div key={bubble.id} className='bubbleContainer'>
-            {bubble.summary !== undefined && (
-              <>
-                {bubble.size === 'large' && <h1>{bubble.name}</h1>}
-                {(bubble.size === 'medium' || !bubble.size) && <h2>{bubble.name}</h2>}
-                {bubble.size === 'small' && <h3>{bubble.name}</h3>}
-                {bubble.link && <Link href={bubble.link}>[More Information]</Link>}
-                <br/>
-                <p>{bubble.summary}</p>
-              </>
-            )}
-            {bubble.summary === undefined && (
-              <div className='image'>
-                <Image
-                  src={'/images/' + bubble.image + '.png'}
-                  alt={bubble.name}
-                  quality={50}
-                  priority={true}
-                  layout='fill'
-                  objectFit='contain'
-                />
-              </div>
-            )}
+      unusedBubbles.splice(centralBubble, 1);
+
+      // Go through layers of heritage.
+      for (let step = 0; step < localBubble.length; step++) {
+        availableChildren = nextAvailableChildren;
+        nextAvailableChildren = [[], []];
+        
+        // Go through the list of bubbles needing a parent.
+        for (let i = 0; i < unusedBubbles.length; i++) {
+          const currentBubble = unusedBubbles[i];
+          
+          // Go through the list of bubbles needing a child.
+          for (let j = 0; j < availableChildren[0].length; j++) {
+            const routeToBubble = availableChildren[0][j];
+            const routeBubble = availableChildren[1][j];
+            
+            // Connect parent with child.
+            if (currentBubble.connection === routeBubble.id) {
+              let bubbleSearch: RecursiveBubble = reformattedBubbles;
+              
+              for (let pathIndex = 0; pathIndex < routeToBubble.length; pathIndex++) {
+                const pathSegment = routeToBubble[pathIndex];
+
+                const newBubbleSearch = bubbleSearch.children.find((e) => e.bubble.id === pathSegment);
+                if (newBubbleSearch) bubbleSearch = newBubbleSearch;
+              }
+              
+              bubbleSearch.children.push({bubble: currentBubble, children: []});
+              nextAvailableChildren[0].push([...routeToBubble, currentBubble.id]);
+              nextAvailableChildren[1].push(currentBubble);
+            }
+          }
+        }
+        if (!nextAvailableChildren[0]) break;
+      }
+
+      setReformattedBubbles(reformattedBubbles);
+    }, [localBubble]);
+
+    return (
+      <div className='accessibilityPage'>
+        <Head>
+          <meta name='theme-color' content={COLOR_MODES.find(e => e.name === 'Light')?.primary} />
+        </Head>
+        <header>
+          <p className='logo'>YSK Kyle</p>
+          <div className='buttonRow'>
+            <Link href='/'>
+              <button className='button'>Home Page</button>
+            </Link>
+            <button className='button' onClick={() => setAccessibility('Visuals')}>To Visual Page</button>
           </div>
-        ))}
-      </main>
-    </div>
-  );
-};
+        </header>
+        <main>
+          {reformattedBubbles && (
+            <InfoBlock recursiveBubble={reformattedBubbles}/>
+          )}
+        </main>
+      </div>
+    );
+  };
 
 export default AccessibilityPage;
