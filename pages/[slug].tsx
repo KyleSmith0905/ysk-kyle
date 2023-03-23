506621e85b806010d3938f3f636b1836bc095034
Kyle Smith <kylesmith090502@gmail.com>
2023-03-23 13:09:31 -0400

    meta: Formatted slug file.

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -11,114 +11,147 @@ import { Button } from '../components/Button';
 import VisualsPage from '../components/VisualsPage';
 
 interface BubblePageProps {
-  slug: string;
-  cookies?: Cookies;
-  isUserBot?: boolean;
-  bubbles: IBubble[];
+	slug: string;
+	cookies?: Cookies;
+	isUserBot?: boolean;
+	bubbles: IBubble[];
 }
 
 const BubblePage:
-  NextPage<BubblePageProps> |
-  FunctionComponent<BubblePageProps> = ({
-    slug, cookies, bubbles: localBubble = [], isUserBot = false
-  }) => {
-  const slugFormatted = slug.split('-').map((s: string) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
-  
-  const reverseBubbles = localBubble.slice().reverse();
-  const [bubbles, setBubbles] = useState<IBubble[]>(structuredClone(reverseBubbles));
-  const [accessibility, setAccessibility] = useState<'Accessibility' | 'Visuals' | 'Undetermined'>(() => {
-    if (isUserBot) return 'Accessibility';
-    return cookies?.accessibility ?? 'Undetermined';
-  });
+	| NextPage<BubblePageProps>
+	| FunctionComponent<BubblePageProps> = ({
+	slug,
+	cookies,
+	bubbles: localBubble = [],
+	isUserBot = false,
+}) => {
+	const slugFormatted = slug
+		.split('-')
+		.map((s: string) => s.charAt(0).toUpperCase() + s.substring(1))
+		.join(' ');
 
-  useEffect(() => {
-    // Center screen to origin.
-    scrollTo(1000 - (window.innerWidth / 2), 1000 - (window.innerHeight / 2));
-    // Display a message in chat.
-    console.log(welcomeMessage());
-  }, []);
+	const reverseBubbles = localBubble.slice().reverse();
+	const [bubbles, setBubbles] = useState<IBubble[]>(
+		structuredClone(reverseBubbles)
+	);
+	const [accessibility, setAccessibility] = useState<
+		'Accessibility' | 'Visuals' | 'Undetermined'
+	>(() => {
+		if (isUserBot) return 'Accessibility';
+		return cookies?.accessibility ?? 'Undetermined';
+	});
 
-  // Uses cookie values to save settings.
-  useEffect(() => {
-    if (cookies && Object.keys(cookies).length === 0) {
-      const accessibilityCookie = getCookie('accessibility') as typeof accessibility;
-      if (accessibilityCookie) setAccessibility(accessibilityCookie);
-    }
-  }, [cookies]);
+	useEffect(() => {
+		// Center screen to origin.
+		scrollTo(1000 - window.innerWidth / 2, 1000 - window.innerHeight / 2);
+		// Display a message in chat.
+		console.log(welcomeMessage());
+	}, []);
 
-  // Saves accessibility settings for next time.
-  useEffect(() => {
-    if (accessibility === 'Undetermined') return;
-    setCookie('accessibility', accessibility);
-  }, [accessibility]);
+	// Uses cookie values to save settings.
+	useEffect(() => {
+		if (cookies && Object.keys(cookies).length === 0) {
+			const accessibilityCookie = getCookie(
+				'accessibility'
+			) as typeof accessibility;
+			if (accessibilityCookie) setAccessibility(accessibilityCookie);
+		}
+	}, [cookies]);
 
-  return (
-    <>
-      <Head>
-        <title>{slugFormatted + ' | YSK Kyle - A portfolio website for Kyle Smith'}</title>
-        <meta property='og:title' content={slugFormatted + ' | YSK Kyle - A portfolio website for Kyle Smith'} />
-        <meta name='twitter:title' content={slugFormatted + ' | YSK Kyle - A portfolio website for Kyle Smith'} />
-      </Head>
-      {accessibility === 'Undetermined' && (
-          <div className='alertContainer'>
-            <div className='alertBox'>
-              <h1 style={{ marginTop: '0rem' }}>Hello Visitor!</h1>
-              <p>This page is highly animated and could create a distraction.</p>
-              <p>Select whether you prefer an accessible or visual experience.</p>
-              <div style={{ marginTop: '2rem' }} className='buttonRow'>
-                <Button onClick={() => setAccessibility('Accessibility')}>Accessibility</Button>
-                <Button onClick={() => setAccessibility('Visuals')}>Visuals</Button>
-              </div>
-            </div>
-          </div>
-        )}
-        {accessibility === 'Accessibility' && (
-          <AccessibilityPage
-            cookies={cookies}
-            slug={slug}
-            bubbles={bubbles}
-            setBubbles={setBubbles}
-            setAccessibility={setAccessibility}
-          />
-        )}
-        {accessibility !== 'Accessibility' && (
-          <VisualsPage
-            cookies={cookies}
-            slug={slug}
-            bubbles={bubbles}
-            setBubbles={setBubbles}
-            setAccessibility={setAccessibility}
-          />
-        )}
-    </>
-  );
+	// Saves accessibility settings for next time.
+	useEffect(() => {
+		if (accessibility === 'Undetermined') return;
+		setCookie('accessibility', accessibility);
+	}, [accessibility]);
+
+	return (
+		<>
+			<Head>
+				<title>
+					{slugFormatted + ' | YSK Kyle - A portfolio website for Kyle Smith'}
+				</title>
+				<meta
+					property='og:title'
+					content={
+						slugFormatted + ' | YSK Kyle - A portfolio website for Kyle Smith'
+					}
+				/>
+				<meta
+					name='twitter:title'
+					content={
+						slugFormatted + ' | YSK Kyle - A portfolio website for Kyle Smith'
+					}
+				/>
+			</Head>
+			{accessibility === 'Undetermined' && (
+				<div className='alertContainer'>
+					<div className='alertBox'>
+						<h1 style={{ marginTop: '0rem' }}>Hello Visitor!</h1>
+						<p>This page is highly animated and could create a distraction.</p>
+						<p>Select whether you prefer an accessible or visual experience.</p>
+						<div style={{ marginTop: '2rem' }} className='buttonRow'>
+							<Button onClick={() => setAccessibility('Accessibility')}>
+								Accessibility
+							</Button>
+							<Button onClick={() => setAccessibility('Visuals')}>
+								Visuals
+							</Button>
+						</div>
+					</div>
+				</div>
+			)}
+			{accessibility === 'Accessibility' && (
+				<AccessibilityPage
+					cookies={cookies}
+					slug={slug}
+					bubbles={bubbles}
+					setBubbles={setBubbles}
+					setAccessibility={setAccessibility}
+				/>
+			)}
+			{accessibility !== 'Accessibility' && (
+				<VisualsPage
+					cookies={cookies}
+					slug={slug}
+					bubbles={bubbles}
+					setBubbles={setBubbles}
+					setAccessibility={setAccessibility}
+				/>
+			)}
+		</>
+	);
 };
 
 export default BubblePage;
 
-export const getServerSideProps: GetServerSideProps = async ({ params, req }) => {
-  let slug = params?.slug ?? '';
-  if (Array.isArray(slug)) slug = slug.join('/');
+export const getServerSideProps: GetServerSideProps = async ({
+	params,
+	req,
+}) => {
+	let slug = params?.slug ?? '';
+	if (Array.isArray(slug)) slug = slug.join('/');
 
-  let allPaths = readdirSync('lib/bubbleData');
+	let allPaths = readdirSync('lib/bubbleData');
 
-  allPaths = allPaths.map(path => path.split('.')[0]);
-  allPaths = allPaths.filter(path => !path.startsWith('_'));
-  allPaths = allPaths.filter(path => path !== '404');
-  allPaths = allPaths.filter(path => path !== 'index');
+	allPaths = allPaths.map((path) => path.split('.')[0]);
+	allPaths = allPaths.filter((path) => !path.startsWith('_'));
+	allPaths = allPaths.filter((path) => path !== '404');
+	allPaths = allPaths.filter((path) => path !== 'index');
 
-  const notFound = !allPaths.includes(slug);
+	const notFound = !allPaths.includes(slug);
 
-  const bubbleDataImport = notFound ? {} : (await import('../lib/bubbleData/' + slug));
-  const bubbles = bubbleDataImport.default;
+	const bubbleDataImport = notFound
+		? {}
+		: await import('../lib/bubbleData/' + slug);
+	const bubbles = bubbleDataImport.default;
 
-  return {
-    props: {
-      slug: params?.slug,
-      cookies: req.cookies,
-      bubbles: bubbles,
-      isUserBot: IsUserBot(req.headers['user-agent']),
-    },
-    notFound: !allPaths.includes(slug),
-  };
+	return {
+		props: {
+			slug: params?.slug,
+			cookies: req.cookies,
+			bubbles: bubbles,
+			isUserBot: IsUserBot(req.headers['user-agent']),
+		},
+		notFound: !allPaths.includes(slug),
+	};
 };
